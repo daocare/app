dda7e014efc4c0d02509131e0d532ce1fc9a8543
Jason Smythe <jason@smythemail.za.net>
2020-03-01 09:37:09 +0100

    deploy again ---

--- twitter/Twitter.js
+++ twitter/Twitter.js
@@ -2,6 +2,8 @@ var Twitter = require('twitter');
 var config = require('./config.js');
 var T = new Twitter(config);
 const Web3 = require('web3');
+const Box = require('3box');
+// const moment = require('moment');
 
 const {
   mnemonic,
@@ -45,20 +47,20 @@ const setupWeb3 = async () => {
 // instanciate contracts
 
 var params = {
-  q: '#ufc',
+  q: '#WhoopTogether',
   count: 3,
   result_type: 'recent',
   lang: 'en',
 };
 
-const voteProxy = async value => {
-  console.log({ value });
-  console.log('the contract', noLossDaoContract);
-  let tx = await noLossDaoContract.methods.doesNothing(value).send({
-    from: mainAddress,
-  });
+const voteProxy = async (proposalId, usersAddress) => {
+  console.log('vote params', { proposalId, usersAddress });
+  let tx = await noLossDaoContract.methods
+    .voteProxy(proposalId, usersAddress)
+    .send({
+      from: mainAddress,
+    });
   console.log({ tx });
-  //await updatBalance();
 };
 
 setInterval(() => console.log("I'm still running"), 3000);
@@ -76,40 +78,81 @@ const proccessedTweets = {};
 
 const start = async () => {
   await setupWeb3();
+
+  console.log({ mainAddress });
   // See search  example from API here
   // https://developer.twitter.com/en/docs/tweets/search/api-reference/get-search-tweets
-  setInterval(
-    () =>
-      T.get('search/tweets', params, function(err, data, response) {
-        // console.log({ err, data, response });
-        if (!err) {
-          for (let i = 0; i < data.statuses.length; i++) {
-            // Check if duplicate tweet we have already processed
-            // Check if the tweet is related to us
+  setInterval(() => {
+    console.log('looking for tweets again');
+    T.get('search/tweets', params, async function(err, data, response) {
+      // console.log({ value: data.statuses });
+      if (!err) {
+        for (let i = 0; i < data.statuses.length; i++) {
+          console.log(data.statuses[i].text);
 
-            // id so we could reply to this tweet with completed etherscan tx or error msg
-            let id = { id: data.statuses[i].id_str };
-            if (!!proccessedTweets[id]) {
-              continue;
-            }
-            proccessedTweets[id] = true;
-            // username to check if they are on our 3box database
-            let username = data.statuses[i].user.screen_name;
-            // scrape their vote from the text
-            let text = data.statuses[i].text;
+          // Check if duplicate tweet we have already processed
+          // Check if the tweet is related to us
 
-            if (i === 0) {
-              voteProxy('20000');
-            }
-
-            console.log(text);
+          // id so we could reply to this tweet with completed etherscan tx or error msg
+          let id = { id: data.statuses[i].id_str };
+          if (!!proccessedTweets[id]) {
+            continue;
           }
-        } else {
-          console.log(err);
+          proccessedTweets[id] = true;
+
+          let utcString = data.statuses[i].created_at;
+          var system_date = new Date(Date.parse(utcString));
+          var user_date = new Date();
+          var diff = Math.floor((user_date - system_date) / 1000);
+          console.log('time passed', diff);
+
+          if (diff > 120) {
+            continue;
+          }
+
+          // username to check if they are on our 3box database
+          let username = data.statuses[i].user.screen_name;
+          // scrape their vote from the text
+          let text = data.statuses[i].text;
+
+          console.log({ text });
+
+          const regexEthAddrs = /(0x[a-fA-F0-9]{40})/g;
+          const foundEthAddrs = text.match(regexEthAddrs);
+
+          const regexProposalId = /~(\d+)/g;
+          const foundProposalId = text.match(regexProposalId);
+          console.log(foundEthAddrs);
+          console.log(foundProposalId);
+
+          if (
+            !!foundEthAddrs &&
+            foundEthAddrs.length > 0 &&
+            !!foundProposalId &&
+            foundProposalId.length > 0
+          ) {
+            const userEthAddress = foundEthAddrs[0];
+            const proposalId = foundProposalId[0].substring(1);
+            console.log('found values', userEthAddress, proposalId);
+
+            const profile = await Box.getProfile(userEthAddress);
+            const verifiedAccounts = await Box.getVerifiedAccounts(profile);
+
+            console.log({ verifiedAccounts });
+            if (i === 0) {
+              voteProxy(proposalId, userEthAddress);
+            }
+          } else {
+            console.log('invalid tweet format');
+          }
+
+          console.log(text);
         }
-      }),
-    3000
-  );
+      } else {
+        console.log(err);
+      }
+    });
+  }, 3000);
 };
 
 start();
