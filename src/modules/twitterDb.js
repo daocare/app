93a75fea0119e65020241dfb0f8cb6f9ad5c470f
Jason Smythe <jason@smythemail.za.net>
2020-04-01 02:11:14 +0200

    Get website 'working' on the new testnet code.

--- src/modules/twitterDb.js
+++ src/modules/twitterDb.js
@@ -1,7 +1,7 @@
 import { firestore } from './firebase';
 
-export const emojiExists = async emoji => {
-  let docRef = firestore.collection('emojis').doc(emoji);
+export const emojiExists = async (emoji, networkSuffix = '') => {
+  let docRef = firestore.collection('emojis' + networkSuffix).doc(emoji);
 
   let docSnapshot = await docRef.get();
   return docSnapshot.exists;
