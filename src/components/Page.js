1b5025c3c3af1b074eca4ca32a29e258635f21fb
denhampreen <denhampreen@gmail.com>
2020-04-06 23:12:21 +0200

    site meta

--- src/components/Page.js
+++ src/components/Page.js
@@ -24,6 +24,32 @@ const Page = (props) => {
       <div {...rest}>
         <Helmet>
           <title>{title}</title>
+          <meta charset="UTF-8" />
+          <meta
+            name="description"
+            content="dao.care - no loss endowment fund"
+          />
+          <meta
+            name="keywords"
+            content="daocare, dao, blockchain, ethereum, no loss fund, endowment, defi"
+          />
+          <meta name="author" content="daocare" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          />
+
+          <meta property="og:title" content={title} />
+          <meta
+            property="og:description"
+            content="dao.care - no loss endowment fund"
+          />
+          <meta property="og:image" content="/assets/logo.svg" />
+          {/* This might have to be a png or jpg */}
+          <meta property="og:url" content="https://dao.care" />
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta property="og:site_name" content={title} />
+          <meta name="twitter:image:alt" content={title} />
         </Helmet>
         {children}
       </div>
