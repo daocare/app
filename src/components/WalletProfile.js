2cbcf8beffa70e041cf2082db58b03a793e0012d
denhampreen <denhampreen@gmail.com>
2020-04-06 23:13:53 +0200

    clean walletprofile

--- src/components/WalletProfile.js
+++ src/components/WalletProfile.js
@@ -10,7 +10,6 @@ import IconButton from '@material-ui/core/IconButton';
 import Tooltip from '@material-ui/core/Tooltip';
 import purple from '@material-ui/core/colors/purple';
 
-// import useRouter from 'utils/useRouter';
 const WalletProfile = (props) => {
   const web3Connect = useWeb3Connect();
   const router = useRouter();
@@ -19,21 +18,12 @@ const WalletProfile = (props) => {
     web3Connect.triggerConnect();
   };
   const handleLogout = async () => {
-    // await logout3Box();
     await web3Connect.resetApp();
     router.history.push('/');
-
-    // setThreeBoxStatus(null);
-    // setThreeBoxConnectionStep(0);
-    // setSaveType("3BO X_SCREEN");
   };
   if (web3Connect.connected && web3Connect.address) {
-    // console.log(web3Connect);
-    // console.log({ address: web3Connect.address });
     return (
       <Container maxWidth="md">
-        {/* <div>Connected: {web3Connect.address}</div>
-        <div>Network: {web3Connect.network}</div> */}
         <div
           style={{
             float: 'right',
@@ -46,20 +36,8 @@ const WalletProfile = (props) => {
             address={web3Connect.address}
             showName={true}
             noTheme={true}
-            // orientation="bottom"
             displayFull={true}
           />
-          {/* <Link
-            component="button"
-            variant="body2"
-            onClick={handleLogout}
-            style={{
-              display: 'block',
-              margin: 'auto',
-            }}
-          >
-            Logout
-          </Link> */}
           <Tooltip title="Disconnect from wallet">
             <IconButton
               style={{ color: purple[50] }}
@@ -75,8 +53,6 @@ const WalletProfile = (props) => {
   }
   return (
     <Container maxWidth="md">
-      {/* <div>Connected: {web3Connect.address}</div>
-        <div>Network: {web3Connect.network}</div> */}
       <div style={{ float: 'right', marginTop: 16, marginBottom: 16 }}>
         <Button
           variant="contained"
