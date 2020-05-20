58498e65f95a189fd0a69d58f6da72f600216705
denhampreen <denhampreen@gmail.com>
2020-05-21 00:08:47 +0200

    save mugggle toggle start

--- src/views/HowItWorks.js
+++ src/views/HowItWorks.js
@@ -1,7 +1,15 @@
 import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/styles';
 import AddIcon from '@material-ui/icons/Add';
-import { Typography, Button, Grid } from '@material-ui/core';
+import {
+  Typography,
+  Button,
+  Grid,
+  FormGroup,
+  FormControlLabel,
+  Switch,
+} from '@material-ui/core';
+
 import useWeb3Connect from '../utils/useWeb3Connect';
 import useRouter from '../utils/useRouter';
 import useInterval from '../utils/useInterval';
@@ -22,28 +30,76 @@ const useStyles = makeStyles((theme) => ({
   },
   gridItem: {
     textAlign: 'left',
-    padding: '0 5px !important',
+    padding: '10px 5px !important',
   },
   numberHighlight: {
-    fontSize: 22,
+    fontSize: 26,
     fontWeight: 'bold',
     color: '#A362A5',
     display: 'inline',
+    textAlign: 'left',
   },
   stepExplainerHeader: {
-    fontSize: 22,
+    fontSize: 24,
     color: '#6850A8',
     fontWeight: 'bold',
+    display: 'inline',
   },
-  stepExplainer: {},
+  stepExplainer: { display: 'inline' },
 }));
 
 const HowItWorks = () => {
   const classes = useStyles();
+  const [howSteps, setHowSteps] = useState([
+    {
+      header: 'Join the fund',
+      explainer:
+        'Deposit DAI into the fund to be part of the DAO. The more you deposit the more voting power you receive.',
+    },
+    {
+      header: 'The fund earns interest',
+      explainer: 'Interest accrues on the DAI in the fund.',
+    },
+    {
+      header: 'Vote',
+      explainer:
+        'Vote on the proposal you feel is most worthy of receiving the interest',
+    },
+    {
+      header: 'Winner',
+      explainer:
+        'At the end of each two week round the proposal with the most votes receives the interest from the fund.',
+    },
+    {
+      header: 'Withdraw all your funds',
+      explainer:
+        'At any point if you want to leave. You can withdraw all of your DAI from the fund',
+    },
+  ]);
+
+  const [isMuggle, setIsMuggle] = React.useState(true);
+
+  const handleChange = (event) => {
+    setIsMuggle(event.target.checked);
+  };
 
   return (
     <Page title="dao.care | How it works">
       <Header />
+      <div className="muggleToggle">
+        <FormControlLabel
+          control={
+            <Switch
+              checked={isMuggle}
+              onChange={handleChange}
+              name="isMuggle"
+              color="primary"
+            />
+          }
+          labelPlacement="start"
+          label="I'm a blockchain wizard"
+        />
+      </div>
       {/* <Typography variant="h5">How it Works</Typography> */}
       <Grid
         container
@@ -51,116 +107,24 @@ const HowItWorks = () => {
         spacing={2}
         className={classes.gridContainer}
       >
-        <Grid item xs={1} md={1} className={classes.gridItemNumber}>
-          <Typography variant="body1" className={classes.numberHighlight}>
-            1.
-          </Typography>
-        </Grid>
-        <Grid item xs={11} md={11} className={classes.gridItem}>
-          <Typography className={classes.stepExplainerHeader}>
-            Join the fund
-          </Typography>
-        </Grid>
-        <Grid item xs={1} md={1} className={classes.gridItem}></Grid>
-        <Grid item xs={11} md={11} className={classes.gridItem}>
-          <Typography variant="body1" className={classes.stepExplainer}>
-            Deposit DAI into the fund to be part of the DAO. The more you
-            deposit the more voting power you receive.
-          </Typography>
-        </Grid>
-      </Grid>
-      <Grid
-        container
-        justify="space-between"
-        spacing={2}
-        className={classes.gridContainer}
-      >
-        <Grid item xs={1} md={1} className={classes.gridItemNumber}>
-          <Typography variant="body1" className={classes.numberHighlight}>
-            2.
-          </Typography>
-        </Grid>
-        <Grid item xs={11} md={11} className={classes.gridItem}>
-          <Typography className={classes.stepExplainerHeader}>
-            The fund earns interest
-          </Typography>
-        </Grid>
-        <Grid item xs={1} md={1} className={classes.gridItem}></Grid>
-        <Grid item xs={11} md={11} className={classes.gridItem}>
-          <Typography variant="body1" className={classes.stepExplainer}>
-            Interest accrues on the DAI in the fund.
-          </Typography>
-        </Grid>
-      </Grid>
-      <Grid
-        container
-        justify="space-between"
-        spacing={2}
-        className={classes.gridContainer}
-      >
-        <Grid item xs={1} md={1} className={classes.gridItemNumber}>
-          <Typography variant="body1" className={classes.numberHighlight}>
-            3.
-          </Typography>
-        </Grid>
-        <Grid item xs={11} md={11} className={classes.gridItem}>
-          <Typography className={classes.stepExplainerHeader}>Vote</Typography>
-        </Grid>
-        <Grid item xs={1} md={1} className={classes.gridItem}></Grid>
-        <Grid item xs={11} md={11} className={classes.gridItem}>
-          <Typography variant="body1" className={classes.stepExplainer}>
-            Vote on the proposal you feel is most worthy of receiving the
-            interest
-          </Typography>
-        </Grid>
-      </Grid>
-      <Grid
-        container
-        justify="space-between"
-        spacing={2}
-        className={classes.gridContainer}
-      >
-        <Grid item xs={1} md={1} className={classes.gridItemNumber}>
-          <Typography variant="body1" className={classes.numberHighlight}>
-            4.
-          </Typography>
-        </Grid>
-        <Grid item xs={11} md={11} className={classes.gridItem}>
-          <Typography className={classes.stepExplainerHeader}>
-            Winner
-          </Typography>
-        </Grid>
-        <Grid item xs={1} md={1} className={classes.gridItem}></Grid>
-        <Grid item xs={11} md={11} className={classes.gridItem}>
-          <Typography variant="body1" className={classes.stepExplainer}>
-            At the end of each two week round the proposal with the most votes
-            receives the interest from the fund.
-          </Typography>
-        </Grid>
-      </Grid>
-      <Grid
-        container
-        justify="space-between"
-        spacing={2}
-        className={classes.gridContainer}
-      >
-        <Grid item xs={1} md={1} className={classes.gridItemNumber}>
-          <Typography variant="body1" className={classes.numberHighlight}>
-            5.
-          </Typography>
-        </Grid>
-        <Grid item xs={11} md={11} className={classes.gridItem}>
-          <Typography className={classes.stepExplainerHeader}>
-            Withdraw all your funds
-          </Typography>
-        </Grid>
-        <Grid item xs={1} md={1} className={classes.gridItem}></Grid>
-        <Grid item xs={11} md={11} className={classes.gridItem}>
-          <Typography variant="body1" className={classes.stepExplainer}>
-            At any point if you want to leave. You can withdraw all of your DAI
-            from the fund
-          </Typography>
-        </Grid>
+        {howSteps.map((step, index) => (
+          <Grid item xs={12} md={12} className={classes.gridItem}>
+            <span style={{ width: '20px' }}>
+              <Typography variant="body1" className={classes.numberHighlight}>
+                {index + 1}.{' '}
+              </Typography>
+            </span>
+            {/* </Grid> */}
+            <Typography className={classes.stepExplainerHeader}>
+              {step.header}
+            </Typography>
+            <br />
+            <span style={{ width: '20px' }} />
+            <Typography variant="body1" className={classes.stepExplainer}>
+              {step.explainer}
+            </Typography>
+          </Grid>
+        ))}
       </Grid>
     </Page>
   );
