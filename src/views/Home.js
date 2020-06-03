23d938419b21f395dc51e5d0a323716646229e92
denhampreen <denhampreen@gmail.com>
2020-06-03 22:40:12 +0200

    home page styling

--- src/views/Home.js
+++ src/views/Home.js
@@ -20,6 +20,12 @@ import EllipsisLoader from '../components/EllipsisLoader';
 import FooterInfo from '../components/FooterInfo';
 
 const useStyles = makeStyles((theme) => ({
+  homeContainer: {
+    display: 'flex',
+    flexFlow: 'column nowrap',
+    justifyContent: 'space-between',
+    height: '100%',
+  },
   button: {
     margin: '20px 32px 20px 32px',
     width: 220,
@@ -32,7 +38,7 @@ const useStyles = makeStyles((theme) => ({
     fontSize: 24,
   },
   numberHighlight: {
-    fontSize: 60,
+    fontSize: 80,
     display: 'inline',
     fontWeight: 'bold',
     color: '#A362A5',
@@ -68,51 +74,55 @@ const Home = () => {
     <Page title="dao.care">
       {/* <button onClick={() => console.log(userDaiDeposit)}>test</button> */}
       <Header />
-      <Typography variant="body1" className={classes.decriptionBlurb}>
-        Deposit your DAI. Let your idle interest support community projects.
-        Vote DAO style on twitter for your favourite project every 2 weeks.
-        Interest from the pool is sent to the chosen community project for 2
-        weeks if selected by the DAO. Withdraw your original DAI at anytime.
-      </Typography>
+      <div className={classes.homeContainer}>
+        <Typography variant="body1" className={classes.decriptionBlurb}>
+          Deposit your DAI. Let your idle interest support community projects.
+          Vote DAO style on twitter for your favourite project every 2 weeks.
+          Interest from the pool is sent to the chosen community project for 2
+          weeks if selected by the DAO. Withdraw your original DAI at anytime.
+        </Typography>
 
-      <Grid container justify="space-between" spacing={2}>
-        <Grid item xs={12} md={4} className={classes.gridItem}>
-          {fundSize > 0 ? (
-            <>
+        <Grid container justify="space-between" spacing={2}>
+          <Grid item xs={12} md={4} className={classes.gridItem}>
+            {fundSize > 0 ? (
+              <>
+                <Typography variant="body1" className={classes.numberHighlight}>
+                  {fundSize}
+                </Typography>
+                <Typography
+                  variant="body1"
+                  className={classes.currencyHighlight}
+                >
+                  {' '}
+                  DAI
+                </Typography>
+              </>
+            ) : (
+              <EllipsisLoader />
+            )}
+            <Typography variant="body1">Fund size</Typography>
+          </Grid>
+          <Grid item xs={12} md={4} className={classes.gridItem}>
+            {numberOfMembers > 0 ? (
               <Typography variant="body1" className={classes.numberHighlight}>
-                {fundSize}
+                {numberOfMembers}
               </Typography>
-              <Typography variant="body1" className={classes.currencyHighlight}>
-                {' '}
-                DAI
+            ) : (
+              <EllipsisLoader />
+            )}
+            <Typography variant="body1">Number of Members</Typography>
+          </Grid>
+          <Grid item xs={12} md={4} className={classes.gridItem}>
+            {proposals.length > 0 ? (
+              <Typography variant="body1" className={classes.numberHighlight}>
+                {proposals.length}
               </Typography>
-            </>
-          ) : (
-            <EllipsisLoader />
-          )}
-          <Typography variant="body1">Fund size</Typography>
-        </Grid>
-        <Grid item xs={12} md={4} className={classes.gridItem}>
-          {proposals.length > 0 ? (
-            <Typography variant="body1" className={classes.numberHighlight}>
-              {proposals.length}
-            </Typography>
-          ) : (
-            <EllipsisLoader />
-          )}
-          <Typography variant="body1">Number of Proposals</Typography>
-        </Grid>
-        <Grid item xs={12} md={4} className={classes.gridItem}>
-          {numberOfMembers > 0 ? (
-            <Typography variant="body1" className={classes.numberHighlight}>
-              {numberOfMembers}
-            </Typography>
-          ) : (
-            <EllipsisLoader />
-          )}
-          <Typography variant="body1">Number of Members</Typography>
-        </Grid>
-        {/* <Grid item xs={12} md={4} className={classes.gridItem}>
+            ) : (
+              <EllipsisLoader />
+            )}
+            <Typography variant="body1">Number of Proposals</Typography>
+          </Grid>
+          {/* <Grid item xs={12} md={4} className={classes.gridItem}>
           <Typography variant="body1" className={classes.numberHighlight}>
             {interestPrev > 0 ? (
               <>
@@ -133,7 +143,7 @@ const Home = () => {
             Previous winner <br /> interest rewarded
           </Typography>
         </Grid> */}
-        {/* <Grid item xs={12} md={4} className={classes.gridItem}>
+          {/* <Grid item xs={12} md={4} className={classes.gridItem}>
           <Typography variant="body1" className={classes.numberHighlight}>
             {interestNext > 0 ? (
               <>
@@ -154,73 +164,23 @@ const Home = () => {
             Next winner <br /> interest reward
           </Typography>
         </Grid> */}
-      </Grid>
+        </Grid>
 
-      <div className={classes.buttonContainer}>
-        <Button
-          variant="contained"
-          color="primary"
-          size="large"
-          className={classes.button}
-          startIcon={<AddIcon />}
-          onClick={() => {
-            if (connected) {
-              router.history.push('/submit-proposal');
-            } else {
-              const connect = async () => {
-                try {
-                  await web3Modal.triggerConnect();
-                  router.history.push('/submit-proposal');
-                } catch {
-                  console.warn('Cancelled connection');
-                }
-              };
-              connect();
-            }
-          }}
-        >
-          Submit Proposal
-        </Button>
-        {userDaiDeposit > 0 ? (
+        <div className={classes.buttonContainer}>
           <Button
             variant="contained"
-            color="secondary"
+            color="primary"
             size="large"
             className={classes.button}
-            startIcon={<WithdrawIcon />}
+            startIcon={<AddIcon />}
             onClick={() => {
               if (connected) {
-                router.history.push('/withdraw');
-              } else {
-                const connect = async () => {
-                  try {
-                    await useWeb3Modal.triggerConnect();
-                    router.history.push('/withdraw');
-                  } catch {
-                    console.warn('Cancelled connection');
-                  }
-                };
-                connect();
-              }
-            }}
-          >
-            Withdraw Funds
-          </Button>
-        ) : (
-          <Button
-            variant="contained"
-            color="secondary"
-            size="large"
-            className={classes.button}
-            startIcon={<DepositIcon />}
-            onClick={() => {
-              if (connected) {
-                router.history.push('/deposit');
+                router.history.push('/submit-proposal');
               } else {
                 const connect = async () => {
                   try {
                     await web3Modal.triggerConnect();
-                    router.history.push('/deposit');
+                    router.history.push('/submit-proposal');
                   } catch {
                     console.warn('Cancelled connection');
                   }
@@ -229,21 +189,72 @@ const Home = () => {
               }
             }}
           >
-            Join Pool
+            Submit Proposal
           </Button>
-        )}
-        <Button
-          variant="outlined"
-          color="primary"
-          size="large"
-          className={classes.button}
-          startIcon={<HowToVoteIcon />}
-          onClick={() => {
-            router.history.push('/proposals');
-          }}
-        >
-          All Proposals
-        </Button>
+          {userDaiDeposit > 0 ? (
+            <Button
+              variant="contained"
+              color="secondary"
+              size="large"
+              className={classes.button}
+              startIcon={<WithdrawIcon />}
+              onClick={() => {
+                if (connected) {
+                  router.history.push('/withdraw');
+                } else {
+                  const connect = async () => {
+                    try {
+                      await useWeb3Modal.triggerConnect();
+                      router.history.push('/withdraw');
+                    } catch {
+                      console.warn('Cancelled connection');
+                    }
+                  };
+                  connect();
+                }
+              }}
+            >
+              Withdraw Funds
+            </Button>
+          ) : (
+            <Button
+              variant="contained"
+              color="secondary"
+              size="large"
+              className={classes.button}
+              startIcon={<DepositIcon />}
+              onClick={() => {
+                if (connected) {
+                  router.history.push('/deposit');
+                } else {
+                  const connect = async () => {
+                    try {
+                      await web3Modal.triggerConnect();
+                      router.history.push('/deposit');
+                    } catch {
+                      console.warn('Cancelled connection');
+                    }
+                  };
+                  connect();
+                }
+              }}
+            >
+              Join Pool
+            </Button>
+          )}
+          <Button
+            variant="outlined"
+            color="primary"
+            size="large"
+            className={classes.button}
+            startIcon={<HowToVoteIcon />}
+            onClick={() => {
+              router.history.push('/proposals');
+            }}
+          >
+            View Proposals
+          </Button>
+        </div>
       </div>
       <FooterInfo />
     </Page>
